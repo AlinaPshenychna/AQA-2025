2552d98e335bc15fef32a19b1df3412aa566493d
Alina Pshenychna <pshenolina@yahoo.com>
2025-09-23 09:12:01 +0300

    complete HW

---
+++ HW_function_6/task1.js
@@ -0,0 +1,14 @@
+function size(width, length) {
+  return width * length;
+}
+console.log(size(2, 3));
+
+const rectanSquare = function (width, length) {
+  return width * length;
+};
+console.log(rectanSquare(4, 5));
+
+const rectangleSquare = (width, length) => {
+  return width * length;
+};
+console.log(rectangleSquare(5, 5));
---
+++ HW_function_6/task2.js
@@ -0,0 +1,6 @@
+function humanAge(age) {
+  return age > 17 ? true : false;
+}
+
+console.log(humanAge(15));
+console.log(humanAge(25));
